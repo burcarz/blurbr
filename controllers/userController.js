9e4f3e49e8ec565ad59c3a4f3dea839d2fd42106
burcar <zacharyburcar@gmail.com>
2022-02-13 15:07:55 -0500

    more small changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,8 +48,8 @@ const userController =  {
         .catch(err => res.status(400).json(err));
     },
     // update user
-    updateUser({ params }, res) {
-        User.findOneAndUpdate({ _id: params.id })
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true })
         .then(dbUserData => {
             if (!dbUserData) {
                 res.status(404).json({ message: 'no user found with this id' });
@@ -67,6 +67,7 @@ const userController =  {
                 res.status(404).json({ message: 'no user found with this id' });
                 return;
             }
+            // return Thought.deleteMany()
             res.json(dbUserData);
         })
         .catch(err => res.status(400).json(err))
